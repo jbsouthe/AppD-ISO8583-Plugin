01e52dfb7461e86adff58707050c0c21641ea5c7
John Southerland <john.southerland@appdynamics.com>
2024-07-22 11:13:30 -0600

    found a better interceptor class and method for the inbound BT

--- src/main/java/com/cisco/josouthe/ISO8583InboundInterceptor.java
+++ src/main/java/com/cisco/josouthe/ISO8583InboundInterceptor.java
@@ -2,6 +2,7 @@ package com.cisco.josouthe;
 
 import com.appdynamics.agent.api.AppdynamicsAgent;
 import com.appdynamics.agent.api.EntryTypes;
+import com.appdynamics.agent.api.Transaction;
 import com.appdynamics.instrumentation.sdk.Rule;
 import com.appdynamics.instrumentation.sdk.SDKClassMatchType;
 import com.appdynamics.instrumentation.sdk.SDKStringMatchType;
@@ -22,24 +23,38 @@ public class ISO8583InboundInterceptor extends MyBaseInterceptor {
 
 
     @Override
-    public Object onMethodBegin (Object invokedObject, String className, String methodName, Object[] paramValues) {
-        return null;
+    public Object onMethodBegin (Object invokedObject, String className, String methodName, Object[] paramValues){
+        Object iosMsg = paramValues[0];
+        Object iMessageSource = paramValues[1];
+        String correlationHeader = (String) getReflectiveObject(iosMsg, getStringReflector, CORRELATION_HEADER_KEY);
+        Transaction transaction = AppdynamicsAgent.startTransactionAndServiceEndPoint("ISO8583 Transaction", correlationHeader, "ISO8583 Transaction", EntryTypes.POJO, true);
+        return transaction;
     }
 
     @Override
     public void onMethodEnd (Object state, Object invokedObject, String className, String methodName, Object[] paramValues, Throwable thrownException, Object returnValue) {
-        String correlationHeader = (String) getReflectiveObject(returnValue, getStringReflector, CORRELATION_HEADER_KEY);
-        AppdynamicsAgent.startTransactionAndServiceEndPoint("ISO8583 Transaction", correlationHeader, "ISO8583 Transaction", EntryTypes.POJO, true);
+        Transaction transaction = (Transaction) state;
+        transaction.end();
     }
 
     @Override
     public List<Rule> initializeRules () {
         List<Rule> rules = new ArrayList<Rule>();
 
+        /*
         rules.add(new Rule.Builder(
                 "com.bellid.vacq.iso8583.netty.NettyISODecoder")
                 .classMatchType(SDKClassMatchType.MATCHES_CLASS)
                 .methodMatchString("decode")
+                .methodStringMatchType(SDKStringMatchType.EQUALS)
+                .build()
+        );
+         */
+
+        rules.add(new Rule.Builder(
+                "com.bellid.vacq.iso8583.common.ISOProcessor")
+                .classMatchType(SDKClassMatchType.IMPLEMENTS_INTERFACE)
+                .methodMatchString("process")
                 .methodStringMatchType(SDKStringMatchType.EQUALS)
                 .build()
         );
